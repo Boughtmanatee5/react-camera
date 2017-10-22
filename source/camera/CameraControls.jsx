74c11521ec890d3d31deb792e54a3b7b21fea724
Boughtmanatee5 <seancostellodp@gmail.com>
2017-10-22 11:40:46 -0400

    add configurations for webpack to load sass

--- source/camera/CameraControls.jsx
+++ source/camera/CameraControls.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CameraControlStyles from './CameraControls.scss';
 
 const CameraControls = (props) => {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,26 @@ module.exports = {
   entry: './source/entry.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'react-camera.js'
+    filename: 'react-camera.js',
   },
   resolve: {
-    extensions: ['.js','.jsx', '.json']
+    extensions: ['.js','.jsx', '.json'],
   },
   module: {
     rules: [
       { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.html/, loader: 'html-loader'}
-    ]
+      { test: /\.html/, loader: 'html-loader' },
+      {
+        test: /\.scss$/, 
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, './source/index.html')})
-  ]
+  ],
 };
